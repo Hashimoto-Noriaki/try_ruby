b31f15498cceeb81a12141d9f018c2eb06af7edf
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-05-30 01:22:40 +0900

    if文の基礎

---
+++ get_up_early.rb
@@ -0,0 +1,14 @@
+puts "昨日寝たのが1:00を過ぎている"
+
+is_late = true
+puts is_late
+
+if is_late
+    puts "はよ寝ろ"
+else
+    puts "ええやん"
+end
+
+# 昨日寝たのが1:00を過ぎている
+# true
+# はよ寝ろ
---
+++ mapsample.rb
@@ -0,0 +1,8 @@
+numbers = [1,2,3,4]
+
+double_numbers =
+numbers.map do |number|
+    number*2
+end
+
+p double_numbers
