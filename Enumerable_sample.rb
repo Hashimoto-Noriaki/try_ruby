9f109335ab488d4e57b30e5590d2e8f3c141add6
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-06-26 23:30:09 +0900

    findを使用

--- Enumerable_sample.rb
+++ Enumerable_sample.rb
@@ -15,4 +15,22 @@ numbers.reject do |number|
 number % 2 == 1
 end
 p even_numbers
-[2, 4]
+#[2, 4]
+
+#findメソッドを使って奇数、偶数を見つける
+numbers = [1,2,3,4,5]
+odd_number =
+numbers.find do |number|
+    number.odd?#number.odd? が真になる最初の奇数を探し
+end
+p odd_number
+#[1]
+
+
+numbers = [1,2,3,4,5]
+even_number =
+numbers.find do |number|
+    number.even?#number.even? は、number が偶数であるかどうかを判定
+end
+p even_number
+#[2]
