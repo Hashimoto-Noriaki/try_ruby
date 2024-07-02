70943194fdf69f1bfe755fda28936b7e17e9c578
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-07-02 23:50:48 +0900

    ハッシュで書き換えや削除を使用

--- hash_sample3.rb
+++ hash_sample3.rb
@@ -6,3 +6,17 @@ result = scores.values_at(:ja, :en)
 
 puts result
 #90 85
+
+man ={name:"トム",age:22,hobby:"baseball"}
+puts man[:name]
+#トム
+
+
+person = {name: "ken", country: "japan", height: 191, weight: 80}
+
+person[:blood]= "B"
+person[:weight]= 78
+person.delete(:country)
+puts person
+
+#{:name=>"ken", :height=>191, :weight=>78, :blood=>"B"}
